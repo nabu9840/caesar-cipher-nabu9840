257502c7aaf02d016c86515609a95456ae6d6c61
Nabaraj bhusal <bhusalnabu1234@gmail.com>
2024-08-16 07:34:44 -0400

    cipher practice

--- cipher.py
+++ cipher.py
@@ -1,1 +1,12 @@
-# add your code here
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+plain_text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+offset = 5
+cipher_text = []
+if offset == 5:
+    shifted_letters = letters[abs(offset):] + letters[: abs(offset)]
+for text in plain_text:
+    for i in range(26):
+        if text == letters[i]:
+            cipher_text.append(shifted_letters[i])
+for item in cipher_text:
+ print(item, end='')
